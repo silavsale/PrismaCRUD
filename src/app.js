74aa8ceb0b8e74493b54f77d800042f5c5573a0a
MoonWalker <silavsale@gmail.com>
2023-04-21 10:42:30 -0400

    add: add routes

--- src/app.js
+++ src/app.js
@@ -1,10 +1,28 @@
 const express = require('express')
 const app = express()
+const { PrismaClient } = require('@prisma/client')
 
+const prisma = new PrismaClient()
 app.use(express.json())
 
-app.get('/', (req, res) => {
-    res.send('hello world')
-  })
+app.get('/', async (req, res) => {
+    try {
+        const allUsers = await prisma.user.findMany();
+        res.json(allUsers);
+      } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'An error occurred while fetching users.' });
+      }
+})
+
+app.post('/', async (req, res) => {
+    try {
+        const newUser = await prisma.user.create({ data: req.body });
+        res.json(newUser);
+      } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'An error occurred while creating the user.' });
+      }
+})
 
 app.listen(3001, () => console.log(`Server running on port ${3001}`))
---
+++ src/prisma/migrations/20230420194441_init/migration.sql
@@ -0,0 +1,20 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "tokenVersion" INTEGER DEFAULT 0,
+    "roleId" INTEGER,
+    "themeId" INTEGER,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_name_key" ON "User"("name");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
